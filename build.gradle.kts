8b5ae5879b4bb33d3894c88a50e7069501d34445
ForceRain <jjim9417@gmail.com>
2022-03-13 23:59:32 +0900

    2022.03.13 service annotation 추가 h2 db 연동 위한 사전 작업 진행

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
--- src/main/kotlin/com/example/WebTest/Crawling/CrawlingServiceImpl.kt
+++ src/main/kotlin/com/example/WebTest/Crawling/CrawlingServiceImpl.kt
@@ -1,8 +1,14 @@
 package com.example.WebTest.Crawling
 
+import org.springframework.stereotype.Service
+
+
+@Service
 class CrawlingServiceImpl : CrawlingService {
+    private lateinit var crawlingList : List<String>
+
     override fun getList() {
-        super.getList()
+        print("크롤링 리스트를 가져옵니다.")
 
     }
 
